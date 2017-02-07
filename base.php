dfa080859cd396f6e3ce4299e9c14822f4d44793
Ryan Niswonger <ryan.niswonger@gmail.com>
2017-02-07 10:16:24 -0800

    Update old IE warning

--- base.php
+++ base.php
@@ -1,16 +1,13 @@
 <?php
-
 use Roots\Sage\Setup;
 use Roots\Sage\Wrapper;
-
 ?>
-
 <!doctype html>
 <html <?php language_attributes(); ?>>
   <?php get_template_part('templates/head'); ?>
   <body <?php body_class(); ?>>
-    <!--[if IE]>
-      <div class="alert alert-warning">
+    <!--[if lt IE 10]>
+      <div class="alert alert-warning" style="position: relative; z-index: 9000;">
         <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'sage'); ?>
       </div>
     <![endif]-->
